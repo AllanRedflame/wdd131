abf02ca378ec2221d6efb83cffb8c8409f79c5de
AllanRedflame <Allanredflame@gmail.com>
2026-09-25 13:27:14 -0600

    Changing file location

--- place/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-const year = new Date().getFullYear();
-const modified = document.lastModified;
-
-let copyright = document.getElementById("currentyear");
-
-document.getElementById("lastModified").innerHTML = "Last updated: " + modified;
-copyright.innerHTML = `&copy; ${year} | David Poulsen | United States`;
